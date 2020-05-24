7699ac28c00a747e5948da460cf77273f79f66ce
unknown <arekmichalski@icloud.com>
2020-05-24 19:56:21 +0200

    lab9

---
+++ ASP.NET_MVC/ASP.NET_MVC/Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of thename of a song is 100 characters!")]
+        public string Name { get; set; }
+
+        public ICollection<Song> SongsList { get; set; }
+
+    }
+}
